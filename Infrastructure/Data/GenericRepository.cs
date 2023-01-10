e43a7947e52d974611351d80a04d39578391a4b9
LexiphanicStroon <nate_reeves@outlook.com>
2023-01-10 16:41:49 -0600

    Generic Repository methods

--- Infrastructure/Data/GenericRepository.cs
+++ Infrastructure/Data/GenericRepository.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Core.Entities;
 using Core.Interfaces;
+using Core.Specifications;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Data
@@ -22,9 +23,25 @@ namespace Infrastructure.Data
             return await _context.Set<T>().FindAsync(id);
         }
 
+
         public async Task<IReadOnlyList<T>> ListAllASync()
         {
             return await _context.Set<T>().ToListAsync();
+        }
+
+        public Task<T> GetEntityWithSpec(ISpecification<T> spec)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec)
+        {
+            throw new NotImplementedException();
+        }
+
+        private IQueryable<T> ApplySpecification(ISpecification<T> spec) 
+        {
+            return SpecificationEvaluator<T>.GetQuery(_context.Set<T>().AsQueryable(), spec);
         }
     }
 }
