b72c77be1dbf3f8881e69f3d09c1bc3155814ef1
LexiphanicStroon <nate_reeves@outlook.com>
2023-01-10 13:34:48 -0600

    Create database at runtime

--- API/Program.cs
+++ API/Program.cs
@@ -1,9 +1,9 @@
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using System.Data.SQLite;
+using Microsoft.Extensions.DependencyInjection;
 using Core.Interfaces;
 var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container.
 
 
@@ -13,8 +13,25 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<StoreContext>(x => x.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope()) {
+    var services = scope.ServiceProvider;
+    var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+    try 
+    {
+        var context = services.GetRequiredService<StoreContext>();
+        await context.Database.MigrateAsync();   
+    }
+    catch (Exception ex) {
+        var logger = loggerFactory.CreateLogger<Program>();
+        logger.LogError(ex, "An error occurred during migration");
+    }
+
+
+}
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
