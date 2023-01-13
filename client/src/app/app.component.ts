25c6c958dde0a901a96e28152eb4f60e8b5e1319
LexiphanicStroon <nate_reeves@outlook.com>
2023-01-12 18:04:44 -0600

    typescript models

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { error } from '@angular/compiler/src/util';
+import { Component, OnInit } from '@angular/core';
+import { IPagination } from './models/pagination';
+import { IProduct } from './models/product';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  title = 'E-commerce';
+  products: IProduct[];
+
+constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('http://localhost:5255/api/products?pageSize=50').subscribe((response: IPagination) => {
+      this.products = response.data;
+    });
+  }
 }
